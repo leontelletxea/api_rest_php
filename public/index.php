d0a153fd304e780db8061dc6f14275deb66ed135
leontelletxea <leontelletxea@gmail.com>
2022-05-26 20:05:47 -0300

    api-rest

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 // Twig
 use Slim\Views\Twig;
-use Slim\Views\TwigMiddleware;
+//use Slim\Views\TwigMiddleware;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/poo/Usuario.php';
@@ -16,7 +16,7 @@ $app = AppFactory::create();
 
 $twig = Twig::create('../src/views', ['cache' => false]);
 
-$app->add(TwigMiddleware::create($app, $twig));
+//$app->add(TwigMiddleware::create($app, $twig));
 
 $app->get('/front-end-login', function(Request $request, Response $response, array $args){
     $view = Twig::fromRequest($request);
@@ -33,10 +33,10 @@ $app->get('/front-end-principal', function(Request $request, Response $response,
     return $view->render($response, 'principal.php', ['titulo' => 'Principal']);
 });
 
-$app->post('/usuarios', \Usuario::class . ':AltaUsuario')
-->add(\MW::class . '::VerificarCorreoBD')
-->add(\MW::class . '::VerificarCorreoClaveVacios')
-->add(\MW::class . ':ValidarParametrosUsuario');
+$app->post('/usuarios', \Usuario::class . ':AltaUsuario');
+// ->add(\MW::class . '::VerificarCorreoBD')
+// ->add(\MW::class . '::VerificarCorreoClaveVacios')
+// ->add(\MW::class . ':ValidarParametrosUsuario');
 
 $app->get('[/]', \Usuario::class . ':ListadoUsuarios');
 // ->add(\MW::class . '::AccedePropietarioUsuario')
@@ -51,20 +51,20 @@ $app->get('/autos', \Auto::class . ':ListadoAutos');
 // ->add(\MW::class . '::AccedeEmpleadoAuto')
 // ->add(\MW::class . ':AccedeEncargadoAuto');
 
-$app->post('/login', \Usuario::class . ':Login')
-->add(\MW::class . '::VerificarParametrosBD')
-->add(\MW::class . '::VerificarCorreoClaveVacios')
-->add(\MW::class . ':ValidarParametrosUsuario');
+$app->post('/login', \Usuario::class . ':Login');
+// ->add(\MW::class . '::VerificarParametrosBD')
+// ->add(\MW::class . '::VerificarCorreoClaveVacios')
+// ->add(\MW::class . ':ValidarParametrosUsuario');
 
 $app->get('/login', \Usuario::class . ':VerificarJWT');
 
-$app->delete('[/]', \Auto::class . '::BorrarAuto')
-->add(\MW::class . '::VerificarPropietario')
-->add(\MW::class . ':VerificarToken');
+$app->delete('[/]', \Auto::class . '::BorrarAuto');
+// ->add(\MW::class . '::VerificarPropietario')
+// ->add(\MW::class . ':VerificarToken');
 
-$app->put('[/]', \Auto::class . '::ModificarAuto')
-->add(\MW::class . ':VerificarEncargado')
-->add(\MW::class . ':VerificarToken');
+$app->put('[/]', \Auto::class . '::ModificarAuto');
+// ->add(\MW::class . ':VerificarEncargado')
+// ->add(\MW::class . ':VerificarToken');
 
 
 $app->run();
